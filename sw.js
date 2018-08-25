6c88987fc605821cf365a9cad0f8b07963132efb
fulldump <gerardooscarjt@gmail.com>
2018-08-25 11:35:39 +0200

    feat: All resources are cached
    In this version, all resources are forced to be cached in
    a very aggresive way, if there is a version cached, it will
    be returned!
    
    Also strict mode has been added to sw.js to enforce good js
    practices.

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
 
+'strict';
+
+const assetsCache = 'assets-cache';
+
 self.addEventListener('install', function(e) {
 	console.log('service-worker-install!!!');
+
+	e.waitUntil(caches.open(assetsCache));
 });
 
 self.addEventListener('activate', function(e) {
@@ -8,9 +14,37 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-	console.log('request:', e.request.url);
 
-	e.respondWith(fetch(e.request));
+	var url = e.request.url;
+
+	e.respondWith(
+		caches.open(assetsCache).then(function(cache) {
+			return cache.match(e.request).then(function(response){
+
+				// If response is cached, it is returned :D
+				if (response) {
+					return response;
+				}
+
+				// If not cached, request is fetched, cached and returned, so
+				// that next time could be available offline.
+				return fetch(e.request).then(function(response) {
+					cache.put(e.request, response.clone());
+					return response;
+				});
+
+			}).catch(function(a) {
+
+				// Something went wrong...
+
+			});
+		}).catch(function() {
+
+			// Something went wrong...
+
+		}),
+	);
+
 });
 
 self.addEventListener('message', function(e) {
