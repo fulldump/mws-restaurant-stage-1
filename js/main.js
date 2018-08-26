dbcc0f7be71fc302d50150c55a792d135c8c1677
fulldump <gerardooscarjt@gmail.com>
2018-08-26 02:08:55 +0200

    refactor: Add service worker to all pages
    Extract service worker registration and use it in all pages.

--- js/main.js
+++ js/main.js
@@ -4,31 +4,6 @@ let restaurants,
 	markers = [];
 
 /**
- * Register service worker
- */
-(function() {
-	// All inside this function scope will be forced to be in strict mode
-	'strict';
-
-	if (!navigator.serviceWorker) return;
-
-	navigator.serviceWorker.register('sw.js', {scope:'/'}).then(function(r){
-
-		console.log('waiting:', r.waiting);
-		console.log('installing', r.installing);
-
-		r.addEventListener('updatefound', function(){
-			// TODO: show toast to allow the user update
-			console.log('update found');
-		});
-
-	}).catch(function() {
-		console.log('problem :_(');
-	});
-
-})();
-
-/**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
---
+++ js/swregister.js
@@ -0,0 +1,26 @@
+// 'strict'; // TODO: uncomment this and check
+
+/**
+ * Register service worker
+ */
+(function() {
+	// All inside this function scope will be forced to be in strict mode
+	'strict';
+
+	if (!navigator.serviceWorker) return;
+
+	navigator.serviceWorker.register('sw.js', {scope:'/'}).then(function(r){
+
+		console.log('waiting:', r.waiting);
+		console.log('installing', r.installing);
+
+		r.addEventListener('updatefound', function(){
+			// TODO: show toast to allow the user update
+			console.log('update found');
+		});
+
+	}).catch(function() {
+		console.log('problem :_(');
+	});
+
+})();
